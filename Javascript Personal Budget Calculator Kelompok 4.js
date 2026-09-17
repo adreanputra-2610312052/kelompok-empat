011a5e8c4d1a40511ca6ad8f2b86e23ca5d7a15f
Panji Jalu Hengkara <panjijaluhengkar42508@gmail.com>
2026-09-17 09:01:02 +0700

    Add files via upload

---
+++ Javascript Personal Budget Calculator Kelompok 4.js
@@ -0,0 +1,42 @@
+//Penulisan judul
+console.log("=== Personal Budget Calculator ===")
+console.log("")
+console.log("")
+
+//Input pendapatan
+Pendapatan = Number(prompt("Masukkan pendapatan anda: Rp."))
+console.log("")
+
+//Input jumlah pengeluaran (Dalam berapa kategori)
+jumlahItem = Number(prompt("Jumlah pengeluaran yang ingin dimasukkan: "))
+
+console.log("")
+
+TotalPengeluaran = 0
+daftarPengeluaran = []
+
+//Input nilai pengeluaran per-kategori
+for (let i = 1; i <= jumlahItem; i++) {
+  let kategori = prompt("Kategori pengeluaran ke-" + i + ": ")
+  let jumlah = Number(prompt("Jumlah pengeluaran untuk " + kategori + ": Rp."))
+console.log("")
+  TotalPengeluaran += jumlah
+  daftarPengeluaran.push({kategori: kategori, jumlah: jumlah})
+}
+
+console.log("")
+console.log ("Total Pengeluaran: Rp." + TotalPengeluaran)
+
+//Penghitungan sisa saldo
+console.log("")
+SisaSaldo = Pendapatan - TotalPengeluaran
+console.log ("Sisa Saldo: Rp." + SisaSaldo)
+
+//Pelabelan status keuangan berdasarkan sisa saldo
+console.log("")
+if (SisaSaldo >= 0.3*Pendapatan) {
+  console.log ("Status keuangan: Baik")
+} else if (SisaSaldo >= 0.1*Pendapatan) {
+  console.log("Status keuangan: Perlu Perhatian")
+} else
+  console.log("Status keuangan: Kritis")
